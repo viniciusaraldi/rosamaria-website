a5356af916044b82d5d740883095c9c2035240ff
Vinicius H Araldi <viniciusheckert14@gmail.com>
2022-12-15 14:53:15 -0300

    third commit

--- rosa_website/urls.py
+++ rosa_website/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('paginas.urls')),
+    path('/', include('paginas.urls')),
     path('contact/',include('contact.urls')),
 ]
 
